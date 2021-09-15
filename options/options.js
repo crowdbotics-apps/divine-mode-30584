05ed4e94bc7bf8dd8d936d72ccf52ae8cefb4f71
Crowdbotics <team@crowdbotics.com>
2021-09-15 20:11:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "divine_mode_30584Identifier",
+  url: "https://divine_mode_30584Identifier.botics.co",
+  api: "https://divine_mode_30584Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
